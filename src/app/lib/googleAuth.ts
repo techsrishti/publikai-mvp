d9913cc737bd865dbc3d1a9101bad925fa4a81d7
Teja_3002 <utejakrishnna@gmail.com>
2025-05-20 02:38:04 +0530

    waitlist google

--- src/app/lib/googleAuth.ts
+++ src/app/lib/googleAuth.ts
@@ -3,7 +3,7 @@ import { google } from 'googleapis';
 export async function googleAuth() {
   const auth = new google.auth.GoogleAuth({
     credentials: {
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     },
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
